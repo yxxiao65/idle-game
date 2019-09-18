8e266ae1edc17a01da55e67a9a5656a469453329
Yikeshan Xiaokaiti <yxxiao65@iMac705-462.local>
2019-09-18 13:28:16 -0400

    summary

--- game.js
+++ game.js
@@ -1,14 +1,36 @@
 var score = 0;
+var food = 10;
+var water = 10;
 
 postToPage(score); // Print to the page
+postToPage(food);
+postToPage(score);
 
-createButton('Button', btnPress); // Make a button
-changeTitle('Clicker Game');
+
+
+changeTitle('Yikeshan Game');
+
+
+createButton('bite', btnPress); // Make a button
+
+createButton('drink', btnPress);
+
+
+
+
+
 
 setInterval(btnPress, 1000);
+
+
+
+
 
 function btnPress() {
   score++;
   postToPage(score);
-}
 
+
+
+
+}
