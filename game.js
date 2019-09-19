ed3417bd02be9078fc0d88f134fc7c1c3c3f99a3
Yikeshan Xiaokaiti <yxxiao65@iMac705-462.local>
2019-09-19 13:20:44 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,45 +1,31 @@
+var score = 1; // Create a variable to store the score in
+
+var title = new Title('Clicker Game!'); // Create a title element
+var button = new Button('Click it!', btnPress);   // Create a button element
+var hidden = new Button('Hello!');
+hidden.hide();
+var hidden2 = new Button(':OvO:');
+hidden2.hide();
+var scr = new Text(score);                    // Create a text element
+
+setInterval(btnPress, 1000);   // Set up a loop
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//var score = 1; // Create a variable to store the score in
-
-//var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
-//hidden.hide();
-//var scr = new Text(score);                    // Create a text element
-
-//setInterval(btnPress, 1000);   // Set up a loop
-
-
-
-
-
-//function btnPress() {
-  //score++;         // Increase the score
-  //scr.edit(score); // Update the page with the new score
-//  if(score > 10) {
-//    hidden.show();
+function btnPress() {
+  score++;         // Increase the score
+  scr.edit(score); // Update the page with the new score
+    if(score > 10) {
+    hidden.show();
+  }
+    //if(score > 50) {
+    //hidden.hide(500);
   //}
-  //if(score > 15) {
-//    hidden.hide(500);
-//  }
-//}
+
+  if(score > 40) {
+  hidden2.show();
+  }
+
+
+
+
+}
